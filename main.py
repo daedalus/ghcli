ad7a696e6e7e67013ad0c011f2bc5e5ac35e77f5
dclavijo <daedalus@users.noreply.github.com>
2015-06-15 09:40:50 -0300

    Pull repos

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def main():
 	parser.add_argument('--username', dest='username', help='username')
 	parser.add_argument('--clone-repos', dest='clonerepos', action='store_true', help='clone repos')
 	parser.add_argument('--list-repos', dest='listrepos', action='store_true', help='list repos')
+	parser.add_argument('--pull-repos', dest='pullrepos', action='store_true', help='list repos')
 
 	args = parser.parse_args()
 	user = args.username
@@ -28,10 +29,13 @@ def main():
 	for i in xrange(len(jsondata)):
 		repo_url = jsondata[i]['clone_url']
 		description = jsondata[i]['description']
+		name = jsondata[i]['name']
 		if args.listrepos:
 			print repo_url,description
 		if args.clonerepos:
 			os.system('git clone %s' % repo_url)
+		if args.pullrepos:
+			os.system('cd %s; git pull' % name)
 
 if __name__ == "__main__":
 	main()
